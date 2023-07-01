7102438a97290798ba9c63d4a87a2a45cb938685
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2023-07-01 23:31:40 +0300

    db2ppxml: Dump custom security attributes

--- db2ppxml.py
+++ db2ppxml.py
@@ -11,6 +11,14 @@ def make_prop(pel, row, prop):
         el.text = str(row[prop])
 
 
+def make_map(pel, rows):
+    mapel = ET.SubElement(pel, "map")
+    for r in rows:
+        enel = ET.SubElement(mapel, "entry")
+        ET.SubElement(enel, "string").text = r["attr_uuid"]
+        ET.SubElement(enel, r["type"]).text = r["value"]
+
+
 def main():
     root = ET.Element("client")
     ET.SubElement(root, "version").text = "56"
@@ -44,6 +52,10 @@ def main():
             make_prop(latest, latest_r, "high")
             make_prop(latest, latest_r, "low")
             make_prop(latest, latest_r, "volume")
+
+        attributes = ET.SubElement(sec, "attributes")
+        attr_rows = dbhelper.select("security_attr", where="security='%s'" % sec_r["uuid"], order="seq")
+        make_map(attributes, attr_rows)
 
         events = ET.SubElement(sec, "events")
 

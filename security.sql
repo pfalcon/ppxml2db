50047bf1ddc9639adb984b630fdaf3d4b31ccede
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2023-07-01 22:16:32 +0300

    security.sql: Security schema

---
+++ security.sql
@@ -0,0 +1,14 @@
+CREATE TABLE security(
+uuid VARCHAR(36) NOT NULL,
+name VARCHAR(128),
+-- Yes, can be absent (dax.xml).
+currencyCode VARCHAR(16),
+note TEXT,
+isin VARCHAR(16),
+tickerSymbol VARCHAR(32),
+wkn VARCHAR(32),
+feed VARCHAR(32),
+isRetired INT NOT NULL DEFAULT 0,
+updatedAt VARCHAR(64) NOT NULL,
+PRIMARY KEY(uuid)
+);

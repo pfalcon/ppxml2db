6691e385f958df09a7fda3c672a0762465484044
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2023-07-02 16:05:33 +0300

    xact.sql: Transaction entry schema
    "transaction" is a keyword in SQL, so can't name a table like that.

---
+++ xact.sql
@@ -0,0 +1,15 @@
+CREATE TABLE xact(
+uuid VARCHAR(36) NOT NULL,
+acctype VARCHAR(10) NOT NULL, -- requires to instantiate AccountTranstion vs PortfolioTransaction, but redundant otherwise
+account VARCHAR(36) NOT NULL REFERENCES account(uuid),
+date VARCHAR(32) NOT NULL,
+currencyCode VARCHAR(16) NOT NULL,
+amount INT NOT NULL,
+security VARCHAR(36) REFERENCES security(uuid),
+shares INT NOT NULL,
+note TEXT,
+updatedAt VARCHAR(64) NOT NULL,
+type VARCHAR(16) NOT NULL,
+PRIMARY KEY(uuid)
+);
+CREATE INDEX xact__account ON xact(account);

f98a7c9e396e622bcfd7d5529749a0dcba6cb98d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2023-07-02 17:33:45 +0300

    account.sql: Merge with portfolio.sql
    This is required to avoid duplicate of other tables to adhere to referential
    integrity. With new scheme reference chain will be:
    
    xact_unit -> xact -> account
    
    If we have account vs portfolio tables, we then need to duplicate xact and
    xact_unit too.

--- portfolio.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE portfolio(
-uuid VARCHAR(36) NOT NULL,
-name VARCHAR(128),
-referenceAccount VARCHAR(36) NOT NULL REFERENCES account(uuid),
-isRetired INT NOT NULL DEFAULT 0,
-updatedAt VARCHAR(64) NOT NULL,
-PRIMARY KEY(uuid)
-);
-#CREATE UNIQUE INDEX portfolio__uuid ON portfolio(uuid);

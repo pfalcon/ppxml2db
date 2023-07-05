3da4695fbb5cfd15923fe63c5c0e6b93f0b6a2ec
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2023-07-05 21:47:21 +0300

    attribute_type.sql: Attribute type schema

---
+++ attribute_type.sql
@@ -0,0 +1,11 @@
+CREATE TABLE attribute_type(
+_id INTEGER NOT NULL,
+id VARCHAR(64) NOT NULL,
+name VARCHAR(64) NOT NULL,
+columnLabel VARCHAR(64) NOT NULL,
+source VARCHAR(128),
+target VARCHAR(128) NOT NULL,
+type VARCHAR(128) NOT NULL,
+converterClass VARCHAR(128) NOT NULL,
+PRIMARY KEY(_id)
+);

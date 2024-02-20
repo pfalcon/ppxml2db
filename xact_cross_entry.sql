b3f67c095529533395bb2d4100af475882769fec
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2024-02-20 19:55:06 +0700

    xact_cross_entry.sql: Some changes to accommodate transfer operations

--- xact_cross_entry.sql
+++ xact_cross_entry.sql
@@ -1,9 +1,11 @@
 CREATE TABLE xact_cross_entry(
-type VARCHAR(12) NOT NULL,
-portfolio VARCHAR(36) NOT NULL REFERENCES account(uuid),
-portfolio_xact VARCHAR(36) NOT NULL REFERENCES xact(uuid),
+type VARCHAR(32) NOT NULL,
+portfolio VARCHAR(36) REFERENCES account(uuid),
+portfolio_xact VARCHAR(36) REFERENCES xact(uuid),
 account VARCHAR(36) NOT NULL REFERENCES account(uuid),
-account_xact VARCHAR(36) NOT NULL REFERENCES xact(uuid)
+account_xact VARCHAR(36) NOT NULL REFERENCES xact(uuid),
+accountFrom VARCHAR(36) REFERENCES account(uuid),
+accountFrom_xact VARCHAR(36) REFERENCES xact(uuid)
 );
 CREATE INDEX xact_cross_entry__portfolio_xact ON xact_cross_entry(portfolio_xact);
 CREATE INDEX xact_cross_entry__account_xact ON xact_cross_entry(account_xact);

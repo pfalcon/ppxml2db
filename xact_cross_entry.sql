5dfa39a1c2bcb23ae3c496925ff63207438a8f7b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2024-07-03 18:50:38 +0300

    xact_cross_entry.sql: Additional fields for "to" transfers, not "from"
    Cross Entries appear to be "attached" to "from" transaction, so use
    unsuffixed fields to record "from" transaction, and use additional
    fields for the "to" part.

--- xact_cross_entry.sql
+++ xact_cross_entry.sql
@@ -4,8 +4,8 @@ portfolio VARCHAR(36) REFERENCES account(uuid),
 portfolio_xact VARCHAR(36) REFERENCES xact(uuid),
 account VARCHAR(36) NOT NULL REFERENCES account(uuid),
 account_xact VARCHAR(36) NOT NULL REFERENCES xact(uuid),
-accountFrom VARCHAR(36) REFERENCES account(uuid),
-accountFrom_xact VARCHAR(36) REFERENCES xact(uuid)
+accountTo VARCHAR(36) REFERENCES account(uuid),
+accountTo_xact VARCHAR(36) REFERENCES xact(uuid)
 );
 CREATE INDEX xact_cross_entry__portfolio_xact ON xact_cross_entry(portfolio_xact);
 CREATE INDEX xact_cross_entry__account_xact ON xact_cross_entry(account_xact);

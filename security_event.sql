33059276c275504f832540da0165b3de24ffb0d5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2024-07-06 14:11:01 +0300

    security_event.sql: Add DB-level primary key

--- security_event.sql
+++ security_event.sql
@@ -1,6 +1,8 @@
 CREATE TABLE security_event(
+_id INTEGER NOT NULL,
 security VARCHAR(36) NOT NULL REFERENCES security(uuid),
 date VARCHAR(36) NOT NULL,
 type VARCHAR(32) NOT NULL,
-details TEXT
+details TEXT,
+PRIMARY KEY(_id)
 );

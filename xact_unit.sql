ca73f486dd420e18dcf89d0d2468259b81774f6c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2024-07-07 01:03:21 +0300

    xact_unit.sql: Add forex_amount, forex_currencyCode columns

--- xact_unit.sql
+++ xact_unit.sql
@@ -2,6 +2,8 @@ CREATE TABLE xact_unit(
 xact VARCHAR(36) NOT NULL REFERENCES xact(uuid),
 type VARCHAR(16) NOT NULL,
 amount INT NOT NULL,
-currencyCode VARCHAR(16) NOT NULL
+currencyCode VARCHAR(16) NOT NULL,
+forex_amount INT,
+forex_currencyCode VARCHAR(16)
 );
 CREATE INDEX xact_unit__xact ON xact_unit(xact);

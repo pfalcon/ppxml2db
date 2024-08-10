ef09ab6561e8cb828824154895e5cd4c88ccc921
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2024-08-10 15:17:37 +0300

    account.sql: Add _order and _xmlid fields

--- account.sql
+++ account.sql
@@ -6,6 +6,8 @@ referenceAccount VARCHAR(36) REFERENCES account(uuid),
 currency VARCHAR(16),
 isRetired INT NOT NULL DEFAULT 0,
 updatedAt VARCHAR(64) NOT NULL,
+_xmlid INT NOT NULL,
+_order INT NOT NULL,
 PRIMARY KEY(uuid)
 );
 #CREATE UNIQUE INDEX account__uuid ON account(uuid);

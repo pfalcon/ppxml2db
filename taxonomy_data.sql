cbd7f84740990c03f02a199075c666674c120f2f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2025-07-04 15:37:17 +0300

    taxonomy_data.sql: Add "type" column
    As "value" can be of different types.

--- taxonomy_data.sql
+++ taxonomy_data.sql
@@ -3,6 +3,7 @@ taxonomy VARCHAR(36) NOT NULL REFERENCES taxonomy(uuid),
 -- Can be NULL for taxonomy-level data
 category VARCHAR(36) REFERENCES taxonomy_category(uuid),
 name VARCHAR(64) NOT NULL,
+type VARCHAR(64) NOT NULL DEFAULT "",
 value VARCHAR(256) NOT NULL
 );
 CREATE INDEX taxonomy_data__taxonomy ON taxonomy_data(taxonomy);

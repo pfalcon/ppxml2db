095615a9678a4ff9751d8adfa6d0023bdde8b87b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2026-01-03 16:47:59 +0700

    config_entry.sql: Increase fields length

--- config_entry.sql
+++ config_entry.sql
@@ -1,6 +1,6 @@
 CREATE TABLE config_entry(
 config_set INT NOT NULL REFERENCES config_set(_id),
-uuid VARCHAR(36),
-name VARCHAR(64),
+uuid VARCHAR(255), -- not really a uuid, more like id
+name VARCHAR(255),
 data TEXT
 );

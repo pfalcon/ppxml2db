851944ed886c8e1cb63842aee6c885c399dec51e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2026-01-03 17:41:23 +0700

    taxonomy.sql: Comment out foreign key on taxonomy_category table
    As this causes circular dependency between two tables, which are possible,
    but pain to work with.

--- taxonomy.sql
+++ taxonomy.sql
@@ -1,6 +1,6 @@
 CREATE TABLE taxonomy(
 uuid VARCHAR(36) NOT NULL,
 name VARCHAR(100) NOT NULL,
-root VARCHAR(36) NOT NULL REFERENCES taxonomy_category(uuid),
+root VARCHAR(36) NOT NULL, -- REFERENCES taxonomy_category(uuid), -- commented out to avoid circular dependency
 PRIMARY KEY(uuid)
 );
